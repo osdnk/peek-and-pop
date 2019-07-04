bcc921396dddd0906fcface82729a5bd23e6e78e
satyajit.happy <satyajit.happy@gmail.com>
2019-07-04 21:29:59 +0200

    chore: fix example app

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,24 +1,13 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-const blacklist = require('metro-config/src/defaults/blacklist');
 const path = require('path');
-const pkg = require('./package.json');
+const escape = require('escape-string-regexp');
+const blacklist = require('metro-config/src/defaults/blacklist');
+const pkg = require('../package.json');
 
-const glob = require('glob-to-regexp');
-
-function getBlacklist() {
-  const nodeModuleDirs = [
-    glob(`${path.resolve(__dirname, '..')}/node_modules/*`),
-  ];
-  return blacklist(nodeModuleDirs);
-}
+const peerDependencies = Object.keys(pkg.peerDependencies);
 
 module.exports = {
+  projectRoot: __dirname,
+  watchFolders: [path.resolve(__dirname, '..')],
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -28,8 +17,11 @@ module.exports = {
     }),
   },
   resolver: {
-    blacklistRE: getBlacklist(),
-    providesModuleNodeModules: Object.keys(pkg.dependencies),
+    blacklistRE: blacklist([
+      new RegExp(
+        `^${escape(path.resolve(__dirname, '..', 'node_modules'))}\\/.*$`
+      ),
+    ]),
+    providesModuleNodeModules: ['@babel/runtime', ...peerDependencies],
   },
-  watchFolders: [path.resolve(__dirname, '..')],
 };
